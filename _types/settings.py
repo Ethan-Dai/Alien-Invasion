6a621b32d01ff91f2433c3f873204d2fe79c6a6f
Ethan Dai <56302589+Ethan-Dai@users.noreply.github.com>
2019-10-13 00:09:28 +0800

    Update settings.py

--- _types/settings.py
+++ _types/settings.py
@@ -10,10 +10,10 @@ class Settings():
         # aliens
         self.alien_max = 10
         self.alien_generate = 15  # 5/1000;
-        self.alien_type0 = 800  #
-        self.alien_type1 = 600
-        self.alien_type2 = 400
-        self.alien_type3 = 200
+        self.alien_type0 = 700  #
+        self.alien_type1 = 400
+        self.alien_type2 = 200
+        self.alien_type3 = 100
         self.alien_type4 = 0
 
         # alien weapons
